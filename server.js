1054a66f2cd5b0b1f338f5e9ac305ad85de6ce58
shyshkovskaliliia <shyshkovska.liliia@chnu.edu.ua>
2026-04-13 13:30:54 +0300

    server

--- server.js
+++ server.js
@@ -2,5 +2,11 @@ const app = require('./app');
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
+
+// Graceful shutdown
+process.on('SIGINT', () => {
+  console.log('\n Server shutting down...');
+  process.exit(0);
 });
